00aa7da2a8380865c3f10109e9da3732b5d10507
Brandon Nguyen <brandonlnguyen0812@gmail.com>
2026-04-21 14:06:14 -0400

    hot-fix patching the location of root for citeseer

--- training/citeseer.py
+++ training/citeseer.py
@@ -6,7 +6,7 @@ from torch_geometric.transforms import NormalizeFeatures
 # Function to load the citeseer dataset and return the graph data and number of classes.
 def load_citeseer():
     # Load the citeseer dataset using PyTorch Geometric's Planetoid class.
-    dataset = Planetoid(root='data/Citeseer', name='Citeseer', transform=NormalizeFeatures())
+    dataset = Planetoid(root='datasets/Citeseer', name='Citeseer', transform=NormalizeFeatures())
     data = dataset[0]  # Get the graph data object from the dataset.
     
     return data, dataset.num_classes  # Return the graph data and the number of classes for classification.
