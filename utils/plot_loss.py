0e9bcfa9b9dd82665a3000335a786e52103b5b5f
Brandon Nguyen <brandonn24@knights.ucf.edu>
2026-04-30 15:44:26 -0400

    updating figure sizes in utils/plot_acc.py, utils/plot_loss.py, and utils/plot_confusion_matrix.py

--- utils/plot_loss.py
+++ utils/plot_loss.py
@@ -7,13 +7,14 @@ import matplotlib.pyplot as plt
 # - title: The title for the plot, indicating the model and configuration.
 # - dataset_name: The name of the dataset used for training, to be included in the plot title and filename.
 def plot_loss(loss_list, epochs, title, dataset_name):
-    plt.figure(figsize=(10, 5))
+    plt.figure(figsize=(3.5, 3))
     plt.plot(range(epochs), loss_list, label='Training Loss')
     plt.xlabel('Epochs')
     plt.ylabel('Loss')
     plt.title(f'{title} - {dataset_name}')
     plt.legend()
     plt.grid()
+    plt.tight_layout()
     plt.savefig(f'figures/{title.replace(" ", "_")}_{dataset_name}_loss_plot.png')
 
     return
